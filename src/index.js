96e551a1dcb6e9f64fcafa0c33f5f4b9d2601a91
Volha Kazlovich <eexmellie@gmail.com>
2019-02-27 18:19:03 +0300

    feat: implement money exchange method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
+
+const coins = {
+  "H": 50,
+  "Q": 25,
+  "D": 10,
+  "N": 5,
+  "P": 1,
+}
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let amountOfMoney = currency;
+  const exchange = {};
+  if (amountOfMoney <= 0) return {};
+  if (amountOfMoney > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+  Object.keys(coins).forEach(coinName => {
+    const coinValue = coins[coinName];
+    if (amountOfMoney < coinValue) return;
+    const coinAmount = Math.floor( amountOfMoney / coinValue);
+    if (coinAmount > 0) {
+      exchange[coinName] = coinAmount;
+      amountOfMoney -= coinValue * coinAmount;
+    }
+  });
+
+  return exchange;
 }
